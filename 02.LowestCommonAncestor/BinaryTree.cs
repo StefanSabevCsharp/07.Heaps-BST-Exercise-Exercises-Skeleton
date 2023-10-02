4820b28160693bf20a062e7a62a213f3c7121dfd
Stefan Sabev <stevenbg91@gmail.com>
2023-10-02 16:42:37 +0200

    Update BinaryTree.cs

--- 02.LowestCommonAncestor/BinaryTree.cs
+++ 02.LowestCommonAncestor/BinaryTree.cs
@@ -36,7 +36,43 @@
 
         public T FindLowestCommonAncestor(T first, T second)
         {
-            throw new NotImplementedException();
+            var firstTree = this.FindNode(first);
+            var secondTree = this.FindNode(second);
+
+            if(firstTree == null || secondTree == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            List<BinaryTree<T>> firstList = new List<BinaryTree<T>>();
+            List<BinaryTree<T>> secondList = new List<BinaryTree<T>>();
+            this.FillTheList(firstTree, firstList);
+            this.FillTheList(secondTree, secondList);
+            var result = firstList.Intersect(secondList).ToList().First();
+            return result.Value;
+        }
+
+        private void FillTheList(BinaryTree<T> firstTree, List<BinaryTree<T>> firstList)
+        {
+           var current = firstTree;
+            while(current != null)
+            {
+                firstList.Add(current);
+                current = current.Parent;
+            }
+        }
+
+        private BinaryTree<T> FindNode(T element)
+        {
+           if(this.Value.Equals(element))
+            {
+                return this;
+            }
+
+            var left = this.LeftChild?.FindNode(element);
+            var right = this.RightChild?.FindNode(element);
+
+            return left ?? right;
         }
     }
 }
